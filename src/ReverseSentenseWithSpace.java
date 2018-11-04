c395271d272ff019b9aad80df401b147475b5774
Menaka <menakaprabu.13@gmail.com>
2018-11-04 00:22:03 -0700

    Commit jewelsstones and reverseSentnceWithSpace programs

---
+++ src/ReverseSentenseWithSpace.java
@@ -0,0 +1,21 @@
+public class ReverseSentenseWithSpace {
+    /**
+     * Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+     Example 1:
+     Input: "Let's take LeetCode contest"
+     Output: "s'teL ekat edoCteeL tsetnoc"
+     Note: In the string, each word is separated by single space and there will not be any extra space in the string.
+     */
+    class Solution {
+        public String reverseWords(String s) {
+            String[] singleWord = s.split(" ");
+            String output ="";
+            for(int i = 0;i<singleWord.length;i++){
+                StringBuffer sb = new StringBuffer(singleWord[i]);
+                output += sb.reverse()+" ";
+            }
+            return output.trim();
+        }
+    }
+}
